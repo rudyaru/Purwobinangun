e5bd2540dc649ff1a6de4db4cdbd4918937447b3
mdickyd <dicky.darmawan256@gmail.com>
2024-11-21 15:42:09 +0700

    finished

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', ' slug', 'body'];
+
+    protected $with = ['author', 'category'];
 
     public function author(): BelongsTo
     {
@@ -19,5 +22,24 @@ class Post extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(category::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query->where('title', 'like', '%' . $search . '%')
+        );
+
+        $query->when(
+            $filters['category'] ?? false,
+            fn ($query, $category) =>
+            $query->whereHas('category', fn($query) => $query->where('slug', $category))
+        );
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas('author', fn($query) => $query->where('username', $author))
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,23 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Informasi', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    return view('posts', ['title' => 'Informasi', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(10)->withQueryString()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
-    
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) . ' Articles by ' .
-    $user->name, 'posts' => $user->posts]);
+        $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
@@ -37,10 +39,9 @@ Route::get('/pengaduan', function () {
 });
 
 Route::get('/faq', function () {
-    return view('faq', [ 'title' => 'FAQ']);
+    return view('faq', ['title' => 'FAQ']);
 });
 
 Route::get('/about', function () {
     return view('about', ['name' => 'Mohammad Dicky Darmawan', 'title' => 'About']);
 });
-
