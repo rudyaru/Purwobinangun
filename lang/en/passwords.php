df76e1db8eb3cec097c41005a404873f1c6809cf
Rudzeus <croyale760@gmail.com>
2024-12-30 12:15:37 +0700

    upload copyan resources/lang/en

---
+++ lang/en/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'reset' => 'Kata sandi Anda telah disetel ulang!',
+    'sent' => 'Kami telah mengirimkan email tautan reset kata sandi Anda!.',
+    'throttled'=> 'Harap tunggu sebelum mencoba lagi.',
+    'token' => 'Tautan pengaturan ulang kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tesebut.',
+
+];
